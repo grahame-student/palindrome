3396bf825c034d316f9d8feab41b50b9463800ad
Grahame White <4whitg16@solent.ac.uk>
2022-07-18 14:57:48 +0100

    applied clang-format

--- src/libPalindrome.cpp
+++ src/libPalindrome.cpp
@@ -12,7 +12,7 @@ bool isPalindrome(std::string inputString)
     inputString = lower(inputString);
 
     uint32_t max_char = inputString.length() - 1;
-    uint32_t mid = inputString.length() / 2;
+    uint32_t mid      = inputString.length() / 2;
 
     for (uint32_t i = 0; i < mid; ++i)
     {
@@ -36,9 +36,9 @@ static std::string removeSpaces(std::string inputString)
 
 static std::string lower(std::string inputString)
 {
-    char result[inputString.length() + 1] = { 0 };
-    uint32_t pos = 0;
-    for(char& c: inputString)
+    char     result[inputString.length() + 1] = {0};
+    uint32_t pos                              = 0;
+    for (char &c : inputString)
     {
         result[pos] = std::tolower(c);
         ++pos;
