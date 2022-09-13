8b54324ab661790d5a345b03739a3a01cb89acd2
Grahame White <57143548+grahame-student@users.noreply.github.com>
2022-09-13 13:10:24 +0100

    test valgrind behaviour

--- src/libPalindrome.cpp
+++ src/libPalindrome.cpp
@@ -16,7 +16,7 @@ bool isPalindrome(std::string inputString)
 
     for (uint32_t i = 0; i < mid; ++i)
     {
-        if (inputString[i] != inputString[(max_char - i)])
+        if (inputString[i] != inputString[(max_char - i - 1)])
         {
             result = false;
             break;
